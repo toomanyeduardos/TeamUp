39cd5d0163d2558c3dee73a0963c6b208c54b0dd
Eduardo Flores <toomanyeduardos@gmail.com>
2017-08-15 21:45:30 -0600

    postviewadapter

--- app/src/main/java/com/eduardoflores/teamup/PostViewAdapter.kt
+++ app/src/main/java/com/eduardoflores/teamup/PostViewAdapter.kt
@@ -1,5 +1,24 @@
 package com.eduardoflores.teamup
 
+import android.content.Context
+import com.eduardoflores.teamup.model.Post
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.google.firebase.database.DatabaseReference
+
 /**
  * Created by eduardo on 8/15/17.
  */
+class PostViewAdapter(modelClass: Class<Post>,
+                      layout: Int,
+                      viewHolderClass: Class<PostHolder>,
+                      databaseReference: DatabaseReference,
+                      context: Context): FirebaseRecyclerAdapter<Post, PostHolder>(modelClass,
+        layout,
+        viewHolderClass,
+        databaseReference) {
+
+    override fun populateViewHolder(viewHolder: PostHolder, model: Post, position: Int) {
+        viewHolder.postTitle.setText(model.title)
+    }
+
+}
